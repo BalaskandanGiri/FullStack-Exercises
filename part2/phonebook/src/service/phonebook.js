829b765fc0d4f8c967991a41e08b7b1ca3e7d205
BalaskandanGiri <balaskandangiri@gmail.com>
2021-05-28 23:37:57 +0530

    url change

--- part2/phonebook/src/service/phonebook.js
+++ part2/phonebook/src/service/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://floating-earth-43351.herokuapp.com/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getPhoneBook = () => {
     return axios.get(baseUrl);
