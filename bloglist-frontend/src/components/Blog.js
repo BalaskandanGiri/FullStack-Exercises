41c32dc4deaded9d8d2c80e798ffcdd7cd1f6fa7
BalaskandanGiri <balaskandangiri@gmail.com>
2021-06-03 09:21:43 +0530

    5.7* Blog list frontend, step7 - added style - added like button

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -2,19 +2,27 @@ import React, {useState} from 'react'
 const Blog = ({blog}) => {
   const [show, setShow] = useState(false)
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   if (!show) {
     return (
-      <div>
+      <div style={blogStyle}>
       Title: {blog.title}, Author: {blog.author} <button onClick={() => {setShow(true)}}>view</button>
       </div> 
     )
   } else {
     return(
-      <div>
-        <p>Title: {blog.title}<button onClick={() => {setShow(false)}}>hide</button></p> 
+      <div style={blogStyle}>
+        <p>Title: {blog.title} &nbsp;<button onClick={() => {setShow(false)}}>hide</button></p> 
         <p>Author: {blog.author}</p>
         <p>url: {blog.url}</p>
-        <p>likes: {blog.likes}</p>
+        <p>likes: {blog.likes} &nbsp;<button>like</button></p>
       </div>
     )
   }
