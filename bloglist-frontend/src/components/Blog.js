364fb81d4d48cfc2e05203c08c717437072d199c
BalaskandanGiri <balaskandangiri@gmail.com>
2021-06-03 09:18:50 +0530

    5.7* Blog list frontend, step7 - Making each blog toggleable to show more info

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,23 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    Title: {blog.title}, Author: {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+  const [show, setShow] = useState(false)
+
+  if (!show) {
+    return (
+      <div>
+      Title: {blog.title}, Author: {blog.author} <button onClick={() => {setShow(true)}}>view</button>
+      </div> 
+    )
+  } else {
+    return(
+      <div>
+        <p>Title: {blog.title}<button onClick={() => {setShow(false)}}>hide</button></p> 
+        <p>Author: {blog.author}</p>
+        <p>url: {blog.url}</p>
+        <p>likes: {blog.likes}</p>
+      </div>
+    )
+  }
+}
 
 export default Blog
