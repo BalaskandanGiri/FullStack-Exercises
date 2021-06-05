3255137e4ad1ce4e9c8d2aee6b172cdfc0b18849
BalaskandanGiri <balaskandangiri@gmail.com>
2021-06-05 17:50:50 +0530

    cypress setup

--- bloglist-frontend/src/test/cypress/blogs.test.js
+++
@@ -1,6 +0,0 @@
-describe('Blog app', function() {
-    beforeEach(function() {
-        cy.request('POST', 'http://localhost:3003/api/testing/reset')
-        cy.visit('http://localhost:3000')
-    })
-})
