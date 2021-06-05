24f2c7f33b36b9c926f6acce50be74bb3586560e
BalaskandanGiri <balaskandangiri@gmail.com>
2021-06-05 18:19:51 +0530

    5.19: bloglist end to end testing, step3 - creating a blog

--- bloglist-frontend/cypress/integration/blogs.spec.js
+++ bloglist-frontend/cypress/integration/blogs.spec.js
@@ -20,12 +20,29 @@ describe('Blog app', function() {
             cy.contains('login').click()
             cy.contains('balaskandan logged in')
         })
-        it.only('Login with username and password', function(){
+        it('Login with username and password', function(){
             cy.get('#username').type('balaskandan')
             cy.get('#password').type('robertdiniro')
             cy.contains('login').click()
             cy.contains('invalid username or password')
             cy.get('.error').should('have.css','color','rgb(255, 0, 0)')
+        })
+    })
+
+    describe('When logged in', function() {
+        beforeEach(function() {
+            cy.get('#username').type('balaskandan')
+            cy.get('#password').type('balaskandan')
+            cy.contains('login').click()
+        })
+
+        it.only('A blog can be created', function() {
+            cy.contains('Create').click()
+            cy.get('#title').type('balaskandan')
+            cy.get('#author').type('balaskandan')
+            cy.get('#url').type('balaskandan')
+            cy.contains('Create').click()
+            cy.contains('Title: balaskandan')
         })
     })
 })
