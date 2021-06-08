2ebc8d69c2001c961bbec294c5c9e18421865a90
BalaskandanGiri <balaskandangiri@gmail.com>
2021-06-08 14:00:07 +0530

    6.4: anecdotes, step2 - creating anecdote

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,10 +5,17 @@ import { useSelector, useDispatch } from 'react-redux'
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-
+  
   const v = (id) => {
     console.log('vote', id)
     dispatch(vote(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(anecdote))
   }
 
   return (
@@ -26,8 +33,8 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={addAnecdote}>
+        <div><input type="text" name="anecdote"/></div>
         <button>create</button>
       </form>
     </div>
