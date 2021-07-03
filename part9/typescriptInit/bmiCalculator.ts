315c6c4c43c601a84f24be894f6187700cb9270e
BalaskandanGiri <balaskandangiri@gmail.com>
2021-07-03 13:57:12 +0530

    9.3 Command line

--- part9/typescriptInit/bmiCalculator.ts
+++ part9/typescriptInit/bmiCalculator.ts
@@ -1,12 +1,12 @@
 const BMI = (height: number, weight: number): string => {
     const bmi = weight/(height*height)
     if (bmi > 25){
-        return "Overweight" + bmi
+        return "Overweight " + bmi
     } else if (bmi < 18) {
-        return "underweight" + bmi
+        return "underweight " + bmi
     } else {
-        return "Normal weight" + bmi
+        return "Normal weight " + bmi
     }
 }
 
-console.log(BMI(1.75, 65))
+console.log(BMI(Number(process.argv[2]), Number(process.argv[3])))
