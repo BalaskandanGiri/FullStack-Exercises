7cf7a9f405b7f96f1fba5d1e33da83cd5bc66d96
BalaskandanGiri <balaskandangiri@gmail.com>
2021-07-04 09:30:53 +0530

    9.5 WebBMI

--- part9/typescriptInit/backend.ts
+++ part9/typescriptInit/backend.ts
@@ -5,6 +5,28 @@ app.get('/ping', (_req: any, resp: { send: (arg0: string) => void; }) => {
     resp.send('Hello Full Stack!')
 })
 
+app.get('/bmi',(req: { query: { height: number; weight: number; }; }, resp: any) => {
+    const height = req.query.height;
+    const weight = req.query.weight;
+    if(isNaN(weight) || isNaN(height)) {
+        throw new Error('malformed input')
+    }
+    const bmi = weight/(height*height)
+    let bmiDesc;
+    if (bmi > 25){
+        bmiDesc =  "Overweight " + bmi
+    } else if (bmi < 18) {
+        bmiDesc = "underweight " + bmi
+    } else {
+        bmiDesc = "Normal weight " + bmi
+    }
+    resp.json({
+        weight: weight,
+        height: height,
+        bmi: bmiDesc
+    })
+})
+
 const PORT = 3003;
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
