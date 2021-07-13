8bb2c70385120e3b8355ca83b49e02f2a949cdbd
BalaskandanGiri <balaskandangiri@gmail.com>
2021-07-13 08:45:24 +0530

    Exercise 9.15. typing with complex types and handling them with switch

--- part9/reacttypescript/src/components/body.tsx
+++ part9/reacttypescript/src/components/body.tsx
@@ -1,13 +1,47 @@
 import React from "react";
+import { CoursePart } from "../types";
 
-type coursePart =  {
-    name: string,
-    exerciseCount: number
-}
+const Body = ({courseParts}: {courseParts: CoursePart[]}) => {
 
-const Body = ({courseParts}: {courseParts: coursePart[]}) => {
-    const res = courseParts.map(x => <p key={x.name}>{x.name}{x.exerciseCount}</p>)
-    return <>{res}</>;
+    return (
+        <div>
+            {courseParts.map((part, i) => {
+                switch(part.type) {
+                    case "normal":
+                        return (<div>
+                            <h2>{part.name} {part.exerciseCount}</h2>
+                            <p>{part.description}</p>
+                        </div>)
+                        break;
+                    case "groupProject":
+                        return (
+                            <div>
+                                <h2>{part.name} {part.exerciseCount}</h2>
+                                <p>Group projects count - {part.groupProjectCount}</p>
+                            </div>
+                        )
+                    case "submission":
+                        return (
+                            <div>
+                                <h2>{part.name} {part.exerciseCount}</h2>
+                                <p>{part.description}</p>
+                                <a href={part.exerciseSubmissionLink}>{part.exerciseCount}</a>
+                            </div>
+                        )
+                    case "special":
+                        return (
+                            <div>
+                                <h2>{part.name} {part.exerciseCount}</h2>
+                                <p>{part.description}</p>
+                                <p>{part.requirements.map((val,i) => <span key={i}>{val} &nbsp;</span>)}</p>
+                            </div>
+                        )
+                    default:
+                        return null;
+                }
+            })}
+        </div>
+    )
 }
 
 export default Body;
---
+++ part9/reacttypescript/src/types.ts
@@ -0,0 +1,29 @@
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+}
+
+interface CoursePartBaseWithDesc extends CoursePartBase{
+    description: string;
+}
+
+interface CourseNormalPart extends CoursePartBaseWithDesc {
+    type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+}
+  
+interface CourseSubmissionPart extends CoursePartBaseWithDesc {
+    type: "submission";
+    exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBaseWithDesc {
+    type: "special";
+    requirements: string[];
+}
+  
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
